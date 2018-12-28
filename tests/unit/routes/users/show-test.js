f59fd7a8c3fcda120a3a3ec61e85c143b85f358f
wei-qiu <wqiu@Weis-Mac-mini.local>
2018-12-28 08:14:01 -0600

    fix user mode test

--- tests/unit/routes/users/show-test.js
+++ tests/unit/routes/users/show-test.js
@@ -4,14 +4,14 @@ import { setupTest } from 'ember-qunit';
 module('Unit | Route | users/show', function(hooks) {
   setupTest(hooks);
 
-  test('it setups book model', function(assert) {
-    let route = this.owner.lookup('route:books/show');
+  test('it setups user model', function(assert) {
+    let route = this.owner.lookup('route:users/show');
     let controller = {};
     let dummyModel = {
-      name: 'foo',
-      price: 100
+      firstName: 'foo',
+      lastName: 'bar'    
     }
     route.setupController(controller, dummyModel);
-    assert.deepEqual(controller.book, dummyModel, 'it setups book model correctly');
+    assert.deepEqual(controller.user, dummyModel, 'it setups users model correctly');
   });
 });
